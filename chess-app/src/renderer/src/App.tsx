e1998d4cd393d585773e8dd34b3d4fea32f99d46
Klay Garcia <klaywork2005@gmail.com>
2026-08-08 14:52:14 -0500

    Initialized ChessTactix repository

---
+++ chess-app/src/renderer/src/App.tsx
@@ -0,0 +1,10 @@
+import './assets/main.css'
+import Background from './components/background'
+
+const App = () => {
+  return (
+  <Background />
+  )
+}
+
+export default App
---
+++ chess-app/src/renderer/src/components/background.tsx
@@ -0,0 +1,8 @@
+const Background = () => {
+  return (
+  <div className="min-h-screen w-full bg-neutral-800" />
+  )
+}
+
+
+export default Background
