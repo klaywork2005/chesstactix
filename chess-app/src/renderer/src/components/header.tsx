e78411b4ecab9914af828cc6f52c2f0c552f3e72
Klay Garcia <klaywork2005@gmail.com>
2026-08-12 19:00:14 -0500

    08/12/2026

--- chess-app/src/renderer/src/components/header.tsx
+++ chess-app/src/renderer/src/components/header.tsx
@@ -6,7 +6,7 @@ const Header = ({ onPlayClick }: HeaderProps) => {
   return (
     <header className="sticky h-24 top-0 z-50 bg-[rgb(15,15,15)] border-b-1 border-neutral-700 text-amber-200">
       <div className="mx-auto flex items-center justify-between">
-        <div className="flex justify-between items-center">
+        <div className="flex justify-start items-center">
           <div
             className="h-24 w-24 bg-amber-200 hover:bg-red-100 cursor-pointer transition-colors"
             style={{
@@ -23,6 +23,13 @@ const Header = ({ onPlayClick }: HeaderProps) => {
           />
           <h1 className="-translate-x-2 select-none text-5xl font-bold">ChessTactix</h1>
           <h1 className="select-none text-m px-2 translate-y-3 text-amber-100">by Klay Garcia</h1>
+        </div>
+        <div className='flex items-center justify-center'>
+              <i className="text-white fa-brands fa-github object-cover text-xl" />
+              <a href="https://github.com/klaywork2005/chesstactix" target="_blank" rel="noopener noreferrer" className="text-xl font-bold text-blue-300 underline underline-offset-6 px-1">
+              GitHub Repository
+              </a>
+
         </div>
 
         <nav className="flex gap-6 px-8">
--- chess-app/src/renderer/src/components/landingpage.tsx
+++ chess-app/src/renderer/src/components/landingpage.tsx
@@ -1,16 +1,28 @@
 const LandingPage = () => {
     return (
         <div className="flex items-center justify-center rounded-lg overflow-hidden border-1 border-neutral-700 bg-neutral-800 w-[85vw] h-[85vh]">
-            <div className='flex flex-col items-center text-lg text-amber-100'>
-                <div>
-                <h1 className="border px-[30vw]">
-                    Welcome to ChessTactix, A Lightweight, Offline Chess Platform
-                </h1>
+
+            <div className="flex flex-col items-center justify-between w-full h-full text-2xl">
+
+                <div className="flex items-center flex-col py-4">
+                    <h1 className="text-center text-amber-100 font-bold pb-2">
+                        WELCOME TO A CROSS-PLATFORM LIGHTWEIGHT, OFFLILNE CHESS APP
+                    </h1>
+                    <h1 className="font-normal text-amber-100/75 pb-2">
+                        Play against a variety of stockfish strengths, analyze openings, analyze any game all locally on your machine
+                    </h1>
+                    <button className="text-amber-900 bg-amber-400 border-1 border-amber-200 rounded-lg py-0.5 px-2">
+                        PLAY NOW
+                    </button>
                 </div>
 
-                <h1>
-                    Play against AI
-                </h1>
+                <div className="flex items-center py-6">
+                    <i className="text-white fa-brands fa-github object-cover text-5xl" />
+                    <a href="https://github.com/klaywork2005/chesstactix" target="_blank" rel="noopener noreferrer" className="text-4xl font-bold text-blue-300 underline underline-offset-12 px-1">
+                    ChessTactix GitHub Repository
+                    </a>
+                </div>
+
             </div>
         </div>
     )
